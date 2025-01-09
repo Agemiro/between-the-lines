7609d252c9fb2dd51d09e9116ea8489f90c687ce
Agemiro Alves de Sousa Neto <agemiroalves11@gmail.com>
2025-01-09 20:02:25 +0100

    feat(category): add category form

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/categories", (req, res) => {
-  res.send("category route");
-});
-
 router.get("/admin/categories/new", (req, res) => {
-  res.send("route for new category");
+  res.render("admin/categories/new");
 });
 
 module.exports = router;
