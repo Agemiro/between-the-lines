f27acd265834786c5253d0e3d543bf2a3d37cfc8
Agemiro Alves de Sousa Neto <agemiroalves11@gmail.com>
2025-01-09 22:02:42 +0100

    feat(category): add delete category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Category = require("./Category");
 const slugify = require("slugify");
+const { where } = require("sequelize");
 
 router.get("/admin/categories/new", (req, res) => {
   res.render("admin/categories/new");
@@ -25,6 +26,25 @@ router.get("/admin/categories", (req, res) => {
   Category.findAll().then((categories) => {
     res.render("admin/categories/index", { categories: categories });
   });
+});
+
+router.post("/categories/delete", (req, res) => {
+  var id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Category.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/admin/categories");
+      });
+    } else {
+      res.redirect("/admin/categories");
+    }
+  } else {
+    res.redirect("/admin/categories");
+  }
 });
 
 module.exports = router;
