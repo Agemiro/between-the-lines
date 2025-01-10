b2daf6a38d06d47520ab4e4437284505165a5398
Agemiro Alves de Sousa Neto <agemiroalves11@gmail.com>
2025-01-10 18:39:01 +0100

    feat(article): add article table

--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize("blog", "root", "#Renault88", {
   host: "localhost",
   dialect: "mysql",
+  timezone: "-03:00",
 });
 
 module.exports = connection;
